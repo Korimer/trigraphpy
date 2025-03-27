fab60e0054189a19dbbfe29cc363a21976cb1ec9
Korimer <thekclov@gmail.com>
2025-03-27 13:40:55 -0600

    cleaned code, polished for release

--- trigraph.py
+++ trigraph.py
@@ -5,10 +5,12 @@ DELIM = ","
 COLWIDTH = .35
 BUFFER = 0.07
 
+# save headings as an array headings, 
+# save values as a 2d array data, where data[i] contains all data in the ith column of the csv
 with open(CSVFILE,"r") as f:
-    headinglist = (f.readline().strip().split(DELIM))
-    cols = len(headinglist)
-    data = [[] for _ in range(len(headinglist))]
+    headings = (f.readline().strip().split(DELIM))
+    cols = len(headings)
+    data = [[] for _ in range(len(headings))]
     for line in f:
         points = [float(i) for i in line.strip().split(DELIM)]
         for i in range(cols):
@@ -20,56 +22,33 @@ ax.grid(axis='y',color='#DDDDDD')
 ax.set_axisbelow(True)
 
 x_indexes = range(1,len(data[0])+1)
+
+# Graph the two bars (each bar composed of 3 sub-bars) per sample.
 barsets = []
-
 for i in range(cols//2):
-    '''for j in range(2):
-        offests = [
-            (x*2) - (COLWIDTH/2) + (COLWIDTH*j)
-            for x in x_indexes
+    for barpos in range(2):
+        adjustedindex = [
+            i-((COLWIDTH+BUFFER)/2)+((COLWIDTH+BUFFER)*barpos)
+            for i in x_indexes
         ]
-        print(offests)
-        print([i-(COLWIDTH/2) for i in x_indexes])
-        print([i+(COLWIDTH/2) for i in x_indexes])
+
         barsets.append(ax.bar(
-            offests,
-            data[(i)+j],
+            adjustedindex,
+            data[(i*2)+barpos],
             width=COLWIDTH,
-        ))'''
-    barsets.append(ax.bar(
-        [i-(COLWIDTH/2)-(BUFFER/2) for i in x_indexes],
-        data[i*2],
-        width=COLWIDTH,
-        edgecolor="black"
-    ))
-    barsets.append(ax.bar(
-        [i+(COLWIDTH/2)+(BUFFER/2) for i in x_indexes],
-        data[(i*2)+1],
-        width=COLWIDTH,
-        edgecolor="black"
-    ))
+            edgecolor="black"
+        ))
 
-'''for bars in barsets:
-    for bar in bars:
-        ax.text(
-            bar.get_x() + bar.get_width() / 2,
-            bar.get_height() + 0.3,
-            round(bar.get_height(), 1),
-            horizontalalignment='center',
-            color=bar.get_facecolor(),
-            weight='bold'
-        )'''
-
-
+# Labels each pair of bars as the appropriate sample.
 ax.set_xticks(
     x_indexes,
     labels=["Sample " + str(i) for i in x_indexes],
     rotation=45
 )
 
-
+# Organizes the legend to pair Given/Tested values.
 ax.legend(
-    labels=headinglist,
+    labels=headings,
     loc='upper center',
     bbox_to_anchor=(0.5, 1),
     ncol=cols/2
@@ -77,4 +56,5 @@ ax.legend(
 
 ax.set_title("Comparison of Given and Tested Soil Proportions")
 ax.set_ylabel("Relative Proportions")
+
 plt.show()
